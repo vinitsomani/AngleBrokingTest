2219a378f1af7813197745d9e2fdd3568c5ff597
AiTechnology <atinder@Atinder-MacBook-Pro.local>
2020-02-29 21:03:33 +0530

    pod instalation done.

--- AngleBrokingTest/AngleBrokingTest/TableCells/HomeTableViewCell.swift
+++ AngleBrokingTest/AngleBrokingTest/TableCells/HomeTableViewCell.swift
@@ -7,30 +7,39 @@
 //
 
 import UIKit
+import SDWebImage
 
 
 class HomeTableViewCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
-
+    
     @IBOutlet weak var userImage: UIImageView!
     
-
     
-
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     func populateData(item: User) {
         self.titleLabel.text = item.display_name
+        
+        guard let urlString = item.avatar_url else {
+            return
+        }
+        if let url = URL.init(string: urlString) {
+            userImage.sd_setImage(with: url) { (image: UIImage?, error: Error?, cacheType: SDImageCacheType, imageURL: URL?) in
+            }
+        }
     }
 }
 
---
+++ AngleBrokingTest/Pods/Target Support Files/Pods-AngleBrokingTest/Pods-AngleBrokingTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AngleBrokingTest : NSObject
+@end
+@implementation PodsDummy_Pods_AngleBrokingTest
+@end
